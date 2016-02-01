a1b6b47c39cd5b74621e29c9ed746ac05a3cad2d
Arel English <arelenglish@gmail.com>
2016-01-31 23:25:04 -0500

    remove scaffold cruft

--- app/controllers/boarding_passes_controller.rb
+++ app/controllers/boarding_passes_controller.rb
@@ -1,24 +1,9 @@
 class BoardingPassesController < ApplicationController
-  before_action :set_boarding_pass, only: [:show, :edit, :update, :destroy]
-
-  # GET /boarding_passes
-  # GET /boarding_passes.json
-  def index
-    @boarding_passes = BoardingPass.all
-  end
-
-  # GET /boarding_passes/1
-  # GET /boarding_passes/1.json
-  def show
-  end
+  before_action :set_boarding_pass, only: [:update]
 
   # GET /boarding_passes/new
   def new
     @boarding_pass = BoardingPass.new
-  end
-
-  # GET /boarding_passes/1/edit
-  def edit
   end
 
   # POST /boarding_passes
@@ -42,31 +27,21 @@ class BoardingPassesController < ApplicationController
     end
   end
 
-  # DELETE /boarding_passes/1
-  # DELETE /boarding_passes/1.json
-  def destroy
-    @boarding_pass.destroy
-    respond_to do |format|
-      format.html { redirect_to boarding_passes_url, notice: 'Boarding pass was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_boarding_pass
+    @boarding_pass = BoardingPass.find(params[:id])
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_boarding_pass
-      @boarding_pass = BoardingPass.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def boarding_pass_params
-      params.require(:boarding_pass).permit(
-        :price,
-        :quantity,
-        :tax_paid,
-        :expiration,
-        :is_valid,
-        :customer_id
-      )
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def boarding_pass_params
+    params.require(:boarding_pass).permit(
+      :price,
+      :quantity,
+      :tax_paid,
+      :expiration,
+      :is_valid,
+      :customer_id
+    )
+  end
 end
--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,13 +2,11 @@ class RatesController < ApplicationController
   before_action :set_rate, only: [:show, :edit, :update, :destroy]
 
   # GET /rates
-  # GET /rates.json
   def index
     @rates = Rate.all
   end
 
   # GET /rates/1
-  # GET /rates/1.json
   def show
   end
 
@@ -34,26 +32,21 @@ class RatesController < ApplicationController
   end
 
   # PATCH/PUT /rates/1
-  # PATCH/PUT /rates/1.json
   def update
     respond_to do |format|
       if @rate.update(rate_params)
         format.html { redirect_to @rate, notice: 'Rate was successfully updated.' }
-        format.json { render :show, status: :ok, location: @rate }
       else
         format.html { render :edit }
-        format.json { render json: @rate.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /rates/1
-  # DELETE /rates/1.json
   def destroy
     @rate.destroy
     respond_to do |format|
       format.html { redirect_to rates_url, notice: 'Rate was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
